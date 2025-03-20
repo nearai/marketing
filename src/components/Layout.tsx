07e506d9b236e95e53a937c44134c3035bead5fe
Caleb Jacob <thecalebjacob@gmail.com>
2025-03-20 11:11:38 -0600

    Simplify layout theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,22 +2,17 @@
 
 import { NearAiUiProvider, Toaster } from '@nearai/ui';
 import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { type ComponentProps, type ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+import { type ReactNode } from 'react';
 
 import { Footer } from './Footer';
 import s from './Layout.module.scss';
 import { Navigation } from './Navigation';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  const params = useSearchParams();
-
   return (
     <NearAiUiProvider
       value={{
-        forcedTheme: params.get('theme') as NonNullable<
-          ComponentProps<typeof NearAiUiProvider>['value']
-        >['forcedTheme'],
         Link,
         useRouter,
       }}
