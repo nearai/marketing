a3a94a2cece16baa27c243b00cc57d48e2c04a59
Caleb Jacob <thecalebjacob@gmail.com>
2025-03-20 11:21:37 -0600

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,14 @@ import { env } from '@/env';
 export const metadata: Metadata = {
   title: {
     template: `%s | NEAR AI`,
-    default: 'NEAR AI - Building the world&apos;s best truly open AI',
+    default: `NEAR AI - Building the world's best truly open AI`,
   },
   description:
     'Join the community building the user-owned future of AI. NEAR AI is focused on creating privacy-preserving, open-source AI that benefits users, not corporations.',
+  twitter: {
+    card: 'summary_large_image',
+    images: [{ url: `${env.NEXT_PUBLIC_BASE_URL}/images/social.jpg` }],
+  },
 };
 
 /*
@@ -27,14 +31,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
-        />
-        <meta
-          property="og:image"
-          content={`${env.NEXT_PUBLIC_BASE_URL}/images/social.jpg`}
-        />
-        <meta
-          name="twitter:image"
-          content={`${env.NEXT_PUBLIC_BASE_URL}/images/social.jpg`}
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
